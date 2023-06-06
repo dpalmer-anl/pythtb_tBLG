2ae4cd4b734c64b9e0b0eb684f2e409976399fde
Daniel Palmer <dpalmer3@login1.summit.olcf.ornl.gov>
2023-06-06 12:17:37 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ Created on Tue Jun  6 10:21:52 2023
 from setuptools import setup, find_packages
 
 setup(
-        name='pythtb_tBLG',
+        name='pythtbtBLG',
         version='0.0.1',
         author="Dan Palmer",
         author_email="dpalmer3@illinois.edu",
         description="python tight binding models for twisted bilayer graphene",
         url="https://github.com/dpalmer-anl/pythtb_tBLG",
-        packages=find_packages(),
+        packages=['pythtbtBLG'],
         include_package_data=True,
         zip_safe=False,
         python_requires=">=3.6, <4",
