5b93d4b36195226085ec4a246c70ecdc9a247d29
Dan Palmer <dpalmer3@illinois.edu>
2023-06-06 11:22:09 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,51 @@ Created on Tue Jun  6 10:21:52 2023
 """
 
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-        name='pythtbtBLG',
-        version='0.0.1',
-        author="Dan Palmer",
-        author_email="dpalmer3@illinois.edu",
-        description="python tight binding models for twisted bilayer graphene",
-        url="https://github.com/dpalmer-anl/pythtb_tBLG",
-        packages=['pythtbtBLG'],
-        include_package_data=True,
-        zip_safe=False,
-        python_requires=">=3.6, <4",
-        install_requires = ["numpy", "scipy", "pandas", "h5py", "ase", "pythtb",
-                            'numba'],
-)
+MAIN_PACKAGE = 'pythtbtBLG'
+DESCRIPTION = "Python tight binding models for twisted bilayer graphene"
+LICENSE = "GPLv3"
+URL = "https://github.com/dpalmer-anl/pythtb_tBLG"
+AUTHOR = "Dan Palmer"
+EMAIL = "dpalmer3@illinois.edu"
+VERSION = '0.0.1'
+CLASSIFIERS = [
+               'Environment :: Console',
+               'Intended Audience :: Science/Research',
+               'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+               'Natural Language :: English',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 3',
+               'Topic :: Scientific/Engineering :: Chemistry',
+               'Topic :: Scientific/Engineering :: Physics']
+DEPENDENCIES = ["numpy", "scipy", "pandas", "h5py", "ase", "pythtb",'numba']
+KEYWORDS = 'physics quantum mechanics solid state'
+
+
+def readme():
+    'Return the contents of the README.md file.'
+    with open('README.rst') as freadme:
+        return freadme.read()
+
+
+def setup_package():
+
+    setup(name=MAIN_PACKAGE,
+          version=VERSION,
+          url=URL,
+          description=DESCRIPTION,
+          author=AUTHOR,
+          author_email=EMAIL,
+          license=LICENSE,
+          keywords=KEYWORDS,
+          long_description=readme(),
+          classifiers=CLASSIFIERS,
+          packages=['pythtbtBLG'],
+          install_requires=DEPENDENCIES,
+          )
+
+
+if __name__ == "__main__":
+    setup_package()
