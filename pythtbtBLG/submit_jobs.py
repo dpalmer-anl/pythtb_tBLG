efb88412aafdef3563e2e48388632a1c3fd86c8c
Dan Palmer <dpalmer3@illinois.edu>
2023-07-31 10:00:05 -0500

    add perlmutter job submission

--- pythtbtBLG/submit_jobs.py
+++ pythtbtBLG/submit_jobs.py
@@ -12,7 +12,8 @@ import matplotlib.pyplot as plt
 import subprocess
 from datetime import datetime
 
-def submit_batch_file(executable,batch_options):
+def submit_batch_file_summit(executable,batch_options,
+                             conda_env ='/ccs/home/dpalmer3/.conda/envs/my_env' ):
 
         sbatch_file="job"+str(hash(datetime.now()) )+".qsub"
         batch_copy = batch_options.copy()
@@ -34,10 +35,34 @@ def submit_batch_file(executable,batch_options):
                 f.write("module load "+m+"\n")
             
             #"jsrun -n1 -c21 -g1 -a1 -EOMP_NUM_THREADS=4 "+
-            f.write("\nsource activate /ccs/home/dpalmer3/.conda/envs/my_env\n")
+            f.write("\nsource activate "+conda_env+"\n")
             #f.write("\nsource activate /ccs/proj/mat221/dpalmer3/conda_envs/summit/cupy-summit\n")
             f.write(executable)
         subprocess.call("bsub -L $SHELL "+sbatch_file,shell=True)
+        
+def submit_batch_file_perlmutter(executable,batch_options,
+                                 conda_env='$PSCRATCH//mypythonev'):
+
+    sbatch_file="job"+str(hash(datetime.now()) )+".sbatch"
+    batch_copy = batch_options.copy()
+
+    prefix="#SBATCH "
+    with open(sbatch_file,"w+") as f:
+        f.write("#!/bin/bash\n")
+
+        modules=batch_copy["modules"]
+
+        for key in batch_copy:
+            if key == "modules":
+                continue
+            f.write(prefix+key+str(batch_copy[key])+"\n")
+
+        for m in modules:
+            f.write("module load "+m+"\n")
+
+        f.write("\nsource activate "+conda_env+"\n")
+        f.write(executable)
+    subprocess.call("sbatch "+sbatch_file,shell=True)
         
 if __name__ =="__main__":
     berry_phase = True
@@ -68,4 +93,4 @@ if __name__ =="__main__":
             executable = "jsrun -n1 -c42 -a1 -r1 -b rs python run_tbmodel.py -d "\
             +atoms_dir+" -o "+label+"_"+str(i)+' -i '+str(i)+' -m '+tbmodel
             +" -od "+outdir+'-c berry'
-            submit_batch_file(executable,batch_options)
+            submit_batch_file_summit(executable,batch_options)
